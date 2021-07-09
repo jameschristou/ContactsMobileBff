be4e05762b4d15537dcad5039748b9ea945b6dd4
James Christou <james.christou@gmail.com>
2021-07-10 09:05:38 +1000

    Adding in ContactDetailsController

---
+++ ContactsMobileBff/Features/ContactDetails/ContactDetailsController.cs
@@ -0,0 +1,50 @@
+﻿using ContactsMobileBff.Features.ContactsListing;
+using ContactsMobileBff.Features.ContactsListing.ComponentBuilders;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ContactsMobileBFF.Features.ContactsListing
+{
+    [ApiController]
+    [Route("contacts/{contactId}")]
+    public class ContactDetailsController : ControllerBase
+    {
+        [HttpGet]
+        public ContactDetailsResponse Get([FromRoute] Guid contactId)
+        {
+            // TODO: create a custom model binder for ContactsListingRequest to handle things like deprecated or invalid enums
+            // sent by client, ensuring NumResults is within bounds
+
+            return new ContactDetailsResponse
+            {
+                Id = contactId,
+                Name = "Test",
+                People = new List<Person>
+                {
+                    new Person { FirstName = "Jimmy", LastName = "Jango", PersonType = PersonType.Primary }
+                }
+            };
+        }
+    }
+
+    public class ContactDetailsResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<Person> People { get; set; }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public PersonType PersonType { get; set; }
+    }
+
+    public enum PersonType
+    {
+        Primary,
+        Other
+    }
+}
